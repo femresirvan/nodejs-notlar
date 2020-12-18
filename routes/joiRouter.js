8304071a0056701911e3d8b076a55843fbcb6a95
femresirvan <femresirvan@gmail.com>
2020-12-18 17:57:43 +0300

    mongo

--- routes/joiRouter.js
+++ routes/joiRouter.js
@@ -1,9 +1,42 @@
 const express = require('express');
-const joiRouter = express.Router();
+const app = express();
 const Joi = require('@hapi/joi');
+const { required, isSchema } = require('@hapi/joi');
+app.use(express.json())
 
-joiRouter.get('/data',(req,res,next)=> {
-    res.send('istek yapildi');
+app.get('/', (req, res, next) => {
+    res.write('istek yapildii');
+    next()
+})
+app.get('/data', (req, res, next) => {
+    res.write('calisti')
+    next()
 })
 
-module.exports = joiRouter;
+const users = []
+app.post('/data', (req, res, next) => {
+    const usersschema = Joi.object({
+        name: Joi.string().required(),
+        password: Joi.number().integer().required().min(4)
+    })
+    const sonuc = usersschema.validate(req.body)
+    if (sonuc.error) {
+        res.send(sonuc.error)
+    } else {
+        const newuser = {
+            name: req.body.name,
+            password: req.body.password
+        }
+        users.push(newuser)
+        res.send(users)
+    }
+    next()
+})
+const mw = (req, res, next) => {
+    console.log("middleware çalışıyo");
+    res.send()
+}
+app.use(mw)
+
+
+app.listen(3000)
